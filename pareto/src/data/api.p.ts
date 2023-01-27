298df991d51f7b048a7d01c510f8b57e6a0d8559
corno <corno.astn.a@schraverus.com>
2023-01-27 19:32:08 +0100

    .

--- pareto/src/data/api.p.ts
+++ pareto/src/data/api.p.ts
@@ -33,6 +33,7 @@ export const $: mmoduleDefinition.TModuleDefinition = {
             "common": "glo-pareto-common",
         }),
         'parameters': d({}),
+        'templates': d({}),
         'types': types({
             "SplitData": group({
                 "value": member(str()),
