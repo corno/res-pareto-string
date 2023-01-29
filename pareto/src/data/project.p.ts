3ee49e7297efd462cef822838a26ef3544fd8493
corno <corno.astn.a@schraverus.com>
2023-01-30 00:57:29 +0100

    .

--- pareto/src/data/project.p.ts
+++ pareto/src/data/project.p.ts
@@ -7,12 +7,24 @@ const d = pr.wrapRawDictionary
 import { $ as api } from "./api.p"
 
 export const $: mproject.TProject = {
-    'type': ['resource', {}],
+    'name': "res-pareto-collation",
+
+    'author': "Corno",
+    'description': "a pareto wrapper around basic string operations as Pareto lacks them (yes, really, by design, the idea is to find a way how not to need them, and if you really do, then use this library)",
+    'license': "ISC",
+
+    'type': ['resource',  {
+        'devDependencies': d({}),
+    }],
     'modules': d({
         "main": {
             'definition': api,
 
         },
     }),
-    'main': "main"
+    'main': "main",
+    'pubdependencies': d({
+        "glo-pareto-common": {},
+    }),
+    'testdependencies': d({}),
 }
