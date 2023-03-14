1203c181f0a0e10cb20ce33b0086c64d04914f93
corno <corno.astn.a@schraverus.com>
2023-03-14 16:52:03 +0100

    .

--- typescript/pub/src/glossary/public.generated.ts
+++ typescript/pub/src/glossary/public.generated.ts
@@ -8,21 +8,21 @@ export namespace B {}
 
 export namespace F {
     
-    export type FromCharacterArray = ($: T.CharacterArray,) => g_common.T.String
+    export type FromCharacterArray = ($: T.CharacterArray) => g_common.T.String
     
-    export type Length = ($: g_common.T.String,) => g_common.T.Number
+    export type Length = ($: g_common.T.String) => g_common.T.Number
     
-    export type ReplaceAll = ($: T.ReplaceAllData,) => g_common.T.String
+    export type ReplaceAll = ($: T.ReplaceAllData) => g_common.T.String
     
-    export type Split = ($: T.SplitData,) => T.SplitResult
+    export type Split = ($: T.SplitData) => T.SplitResult
     
-    export type SplitIn2 = ($: T.SplitData,) => T.SplitIn2Result
+    export type SplitIn2 = ($: T.SplitData) => T.SplitIn2Result
     
-    export type StartsWith = ($: T.StartsWithData,) => g_common.T.Boolean
+    export type StartsWith = ($: T.StartsWithData) => g_common.T.Boolean
     
-    export type Substr = ($: T.SubstrData,) => g_common.T.String
+    export type Substr = ($: T.SubstrData) => g_common.T.String
     
-    export type ToCharacterArray = ($: g_common.T.String,) => T.CharacterArray
+    export type ToCharacterArray = ($: g_common.T.String) => T.CharacterArray
     
-    export type TrimEnd = ($: g_common.T.String,) => g_common.T.String
+    export type TrimEnd = ($: g_common.T.String) => g_common.T.String
 }
