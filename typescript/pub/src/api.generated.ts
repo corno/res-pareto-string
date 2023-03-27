968061ef198f2b5e63a19610b888af4381919328
corno <corno.astn.a@schraverus.com>
2023-03-27 13:44:47 +0200

    .

--- typescript/pub/src/api.generated.ts
+++ typescript/pub/src/api.generated.ts
@@ -3,6 +3,19 @@ import * as pt from 'pareto-core-types'
 import * as g_common from "glo-pareto-common"
 import * as g_this from "./glossary"
 
+export namespace D {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
 export namespace A {
     
     export type createStringBuilder = ($: g_common.T.String, ) => g_this.ASYNC.A.C.CreateStringBuilder
